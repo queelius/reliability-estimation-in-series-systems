692ea62420e763312f6959850b1bf8a9731679b6
queelius <lex@metafunctor.com>
2023-07-30 19:13:39 -0500

    updates

---
+++ changes.md
@@ -0,0 +1,10 @@
+
+# Change log for the paper
+
+## Identifiability section
+
+Corrected an error in the identifiabiltiy section. I meant to say I changed component 5 to have a lower MTTF,
+but mistakenly said component 1. This is a problem because we were plotting the profile of the log-likelihood 
+function for shape parameter 1 of component 1, which is not identifiable, but would have been identifiable
+if we had changed the MTTF of component 1 to be much smaller. I have corrected this error in the paper,
+and also added more clarification about what the profile plot is showing.
--- results/non-unique-mle/fail-test-flat-likelihood.R
+++ results/non-unique-mle/fail-test-flat-likelihood.R
@@ -1,3 +1,8 @@
+# Load necessary libraries
+library(ggplot2)
+library(gridExtra)
+library(latex2exp) # for LaTeX in captions
+library(grid) # for textGrob
 library(tidyverse)
 library(stats)
 library(md.tools)
@@ -5,36 +10,73 @@ library(wei.series.md.c1.c2.c3)
 
 theta <- c(shape1 = 1.2576, scale1 = 994.3661,
            shape2 = 1.1635, scale2 = 908.9458,
-           shape3 = 1.1308, scale3 = 840.1141,
-           shape4 = 1.1802, scale4 = 940.1141,      
-           shape5 = 1.3311, scale5 = 1.1123)
+           shape3 = 1.1308, scale3 = 4.1141)
 
 shapes <- theta[seq(1, length(theta), 2)]
 scales <- theta[seq(2, length(theta), 2)]
-parscale <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
-stopifnot(length(parscale) == length(theta))
 
-n <- 100
-p <- 0
-q <- 1
-
+n <- 30
+p <- .215
+q <- .825
+set.seed(123521)
 tau <- wei.series.md.c1.c2.c3::qwei_series(p = q, scales = scales, shapes = shapes)
 df <- wei.series.md.c1.c2.c3::generate_guo_weibull_table_2_data(
     shapes = shapes, scales = scales, n = n, p = p, tau = tau)
+
+df.tweaked <- df
 sol <- mle_lbfgsb_wei_series_md_c1_c2_c3(
-    theta0 = runif(10,1,100), df = df, hessian = FALSE,
-    control = list(maxit = 1000, parscale = parscale))
+    theta0 = runif(6,1,100), df = df, hessian = FALSE,
+    control = list(maxit = 1000))
+sol.tweaked <- mle_lbfgsb_wei_series_md_c1_c2_c3(
+    theta0 = runif(6,1,100), df = df.tweaked, hessian = FALSE,
+    control = list(maxit = 1000))
+
 l <- Vectorize(function(shape1) {
     theta <- sol$par
     theta[1] <- shape1
     wei.series.md.c1.c2.c3::loglik_wei_series_md_c1_c2_c3(df, theta)
 }, vectorize.args = "shape1")
-shape1 <- seq(9.28, 26, by=.01)
+l.tweaked <- Vectorize(function(shape1) {
+    theta <- sol$par
+    theta[1] <- shape1
+    wei.series.md.c1.c2.c3::loglik_wei_series_md_c1_c2_c3(df.tweaked, theta)
+}, vectorize.args = "shape1")
+
+shape1 <- seq(0.001, 10, by=.01)
+yrange <- range(c(l(shape1), l.tweaked(shape1)))
+
+# Define the true value and MLE for k1
+true_k1 <- shapes[1]
+mle_k1_left <- sol$par[1]
+mle_k1_right <- sol.tweaked$par[1]
+
+caption1 <- "Red dashed line: An MLE for Shape Parameter of Component 1.\nNon-unique MLE."
+caption2 <- "Red dashed line: The MLE of Shape Parameter of Component 1.\nUnique MLE with a simple tweak to data."
 
 plt <- ggplot(data.frame(x=shape1, y=l(shape1)), aes(x=x, y=y)) +
     geom_line() +
-    labs(x="Shape 1", y="Log-likelihood",
-    title="Log-likelihood Profile vs Shape 1") + theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
+    geom_vline(xintercept = true_k1, linetype="dashed", color = "blue", size=1) + # Add true k1
+    geom_vline(xintercept = mle_k1_left, linetype="dashed", color = "red", size=1) + # Add MLE for k1
+    labs(x=TeX("Shape Parameter for Component 1 ($k_1$)"), y="Log-likelihood", caption = caption1) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.caption = element_text(hjust = 0.5, size = 10),
+          plot.margin = margin(1, 1, 5, 1, "cm") # Larger bottom margin
+    ) +    coord_cartesian(ylim = yrange) # Define y range
 
-ggsave("fail-test-flat-likelihood.pdf", plot=plt, device="pdf", width=4, height=3, units="in", dpi=75)
+plt.tweaked <- ggplot(data.frame(x=shape1, y=l.tweaked(shape1)), aes(x=x, y=y)) +
+    geom_line() +
+    geom_vline(xintercept = true_k1, linetype="dashed", color = "blue", size=1) + # Add true k1
+    geom_vline(xintercept = mle_k1_right, linetype="dashed", color = "red", size=1) + # Add MLE for k1
+    labs(x=TeX("Shape Parameter for Component 1 ($k_1$)"), y="Log-likelihood", caption = caption2) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.caption = element_text(hjust = 0.5, size = 10),
+          plot.margin = margin(1, 1, 5, 1, "cm") # Larger bottom margin
+    ) +    
+    coord_cartesian(ylim = yrange) # Define y range
+
+shared_title <- textGrob("Log-likelihood Profile vs Shape Parameter for Component 1: Non-unique MLE vs Unique MLE.\nRed Dashed Line is the True Shape Value.", gp=gpar(fontsize=16))
+
+plt.combined <- arrangeGrob(plt, plt.tweaked, ncol=2, top=shared_title) # use arrangeGrob to combine plots
+ggsave("fail-test-flat-likelihood.pdf", plot=plt.combined, device="pdf", width=10, height=5, units = "in")
