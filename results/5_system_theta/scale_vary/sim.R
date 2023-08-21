6cd2b2642822a914430c8753519d866f5a29c4ba
queelius <queelius@gmail.com>
2023-08-21 01:55:45 -0500

    updated results/5_system_theta/bca/basement/sim.R from kit, added some new sim result dirs

--- results/5_system_theta/scale_vary/sim.R
+++ results/5_system_theta/scale_vary/sim.R
@@ -9,53 +9,55 @@ library(wei.series.md.c1.c2.c3)
 
 theta <- c(shape1 = 1.2576, scale1 = 994.3661,
            shape2 = 1.1635, scale2 = 908.9458,
-           shape3 = 1.1308, scale3 = 840.1141,
+           shape3 = 1.1308, scale3 = NA,
            shape4 = 1.1802, scale4 = 940.1342,
            shape5 = 1.2034, scale5 = 923.1631)
 
-shapes3 <- rep(c(0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9), 1000)
+scales3 <- c(250, 500, 750, 1000, 1250, 1500, 1750)
+scales3 <- rep(scales3, 100)
+
 N <- c(100)
 P <- c(0.215)
 Q <- c(0.825)
-R <- 5  # 9 aug 23 alton office  building B across alton memorial parking 
+R <- 2  # 9 aug 23 alton office  building B across alton memorial parking 
 B <- 1000
 max_iter <- 100L
 max_boot_iter <- 125L
-n_cores <- 3L
+n_cores <- 2L
 
-experiment.name <- "boot-prob-shape1-vary-unmasked"
+experiment.name <- "boot-prob-scale3-vary"
 csv.out <- paste0(experiment.name, ".csv")
 
 probs3_fn <- list()
 probs1_fn <- list()
 
-for (shape3 in shapes3) {
-    theta["shape3"] <- shape3
+for (scale3 in scales3) {
+    theta["scale3"] <- scale3
     shapes <- theta[seq(1, length(theta), 2)]
     scales <- theta[seq(2, length(theta), 2)]
     tryCatch({
-        probs1_fn[[as.character(shape3)]] <- wei.series.md.c1.c2.c3::wei_series_cause(k = 1L, shapes = shapes, scales = scales)
-        probs3_fn[[as.character(shape3)]] <- wei.series.md.c1.c2.c3::wei_series_cause(k = 3L, shapes = shapes, scales = scales)
+        probs1_fn[[as.character(scale3)]] <- wei.series.md.c1.c2.c3::wei_series_cause(k = 1L, shapes = shapes, scales = scales)
+        probs3_fn[[as.character(scale3)]] <- wei.series.md.c1.c2.c3::wei_series_cause(k = 3L, shapes = shapes, scales = scales)
     },
     error = function(e) {
-        probs1_fn[[as.character(shape3)]] <<- wei.series.md.c1.c2.c3::wei_series_cause(k = 1L, shapes = shapes, scales = scales,
+        probs1_fn[[as.character(scale3)]] <<- wei.series.md.c1.c2.c3::wei_series_cause(k = 1L, shapes = shapes, scales = scales,
             mc = TRUE, n = 200000L)
-        probs3_fn[[as.character(shape3)]] <<- wei.series.md.c1.c2.c3::wei_series_cause(k = 3L, shapes = shapes, scales = scales,
+        probs3_fn[[as.character(scale3)]] <<- wei.series.md.c1.c2.c3::wei_series_cause(k = 3L, shapes = shapes, scales = scales,
             mc = TRUE, n = 200000L)
     })
 }
 
 options(digits = 5, scipen = 999)
-for (shape3 in shapes3) {
+for (scale3 in scales3) {
     for (n in N) {
         for (p in P) {
             for (q in Q) {
 
-                theta["shape3"] <- shape3
+                theta["scale3"] <- scale3
                 shapes <- theta[seq(1, length(theta), 2)]
                 scales <- theta[seq(2, length(theta), 2)]
 
-                tau <- wei.series.md.c1.c2.c3::qwei_series(p = q, scales = scales, shapes = shapes)
+                tau <- qwei_series(p = q, scales = scales, shapes = shapes)
                 shapes.mle <- matrix(NA, nrow = R, ncol = length(theta) / 2)
                 scales.mle <- matrix(NA, nrow = R, ncol = length(theta) / 2)
 
@@ -70,7 +72,7 @@ for (shape3 in shapes3) {
                     retry <- FALSE
                     tryCatch({
                         repeat {
-                            df <- wei.series.md.c1.c2.c3::generate_guo_weibull_table_2_data(
+                            df <- generate_guo_weibull_table_2_data(
                                 shapes = shapes, scales = scales, n = n, p = p, tau = tau)
 
                             sol <- mle_lbfgsb_wei_series_md_c1_c2_c3(
@@ -83,12 +85,10 @@ for (shape3 in shapes3) {
                         }
 
                         mle_solver <- function(df, i) {
-                            solb <- mle_lbfgsb_wei_series_md_c1_c2_c3(
+                            mle_lbfgsb_wei_series_md_c1_c2_c3(
                                 theta0 = sol$par, df = df[i, ], hessian = FALSE,
-                                control = list(maxit = max_boot_iter, parscale = sol$par))
-                            solb$par
+                                control = list(maxit = max_boot_iter, parscale = sol$par))$par
                         }
-
                         sol.boot <- boot::boot(df, mle_solver,
                             R = B, parallel = "multicore", ncpus = n_cores)
                     },
@@ -131,13 +131,13 @@ for (shape3 in shapes3) {
                     n = rep(n, R),
                     p = rep(p, R),
                     q = rep(q, R),
-                    prob3 = rep(probs3_fn[[as.character(shape3)]], R),
-                    prob1 = rep(probs1_fn[[as.character(shape3)]], R),
+                    prob3 = rep(probs3_fn[[as.character(scale3)]], R),
+                    prob1 = rep(probs1_fn[[as.character(scale3)]], R),
                     mttf3 = rep(wei.series.md.c1.c2.c3::wei_mttf(shape = shapes[3], scale = scales[3]), R),
                     mttf1 = rep(wei.series.md.c1.c2.c3::wei_mttf(shape = shapes[1], scale = scales[1]), R),
                     mttf = rep(wei.series.md.c1.c2.c3::wei_series_mttf(shapes = shapes, scales = scales), R),
                     tau = rep(tau, R),
-                    shapes1 = rep(shape3, R),
+                    scale3 = rep(scale3, R),
                     shape.mles = shapes.mle,
                     scale.mles = scales.mle,
                     shapes.lower = shapes.lower,
