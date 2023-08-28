b74a3aaac1d97061f90f48ebbe35234f0ccbfbef
queelius <lex@metafunctor.com>
2023-08-28 01:24:04 -0500

    updates

--- results/5_system_samp_size/data-boot-bca-n.R
+++ results/5_system_samp_size/data-boot-bca-n.R
@@ -16,9 +16,9 @@ theta <- c(shape1 = 1.2576, scale1 = 994.3661,
 shapes <- theta[seq(1, length(theta), 2)]
 scales <- theta[seq(2, length(theta), 2)]
 
-csv_file <- "data-boot-bca-n.csv"
+csv_file <- "data-boot-bca-n-large.csv"
 
-N <- rep(c(500, 750), 200)
+N <- rep(c(750, 1000, 2500, 5000), 200)
 P <- c(.215)
 Q <- c(.825)
 R <- 2
@@ -100,7 +100,7 @@ for (n in N) {
                 logliks[iter] <- sol$value
 
                 tryCatch({
-                    ci <- confint(mle_boot(sol.boot), type = "bca", level = 0.95)
+                    ci <- confint(mle_boot(sol.boot), type = "basic", level = 0.95)
                     shapes.ci <- ci[seq(1, length(theta), 2), ]
                     scales.ci <- ci[seq(2, length(theta), 2), ]
                     shapes.lower[iter, ] <- shapes.ci[, 1]
