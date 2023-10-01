534bb7075e80598404a197d8107fc17943feaee6
queelius <lex@metafunctor.com>
2023-10-01 07:32:46 -0500

    updates

--- results/5_system_prob_mask/sim-p.R
+++ results/5_system_prob_mask/sim-p.R
@@ -8,12 +8,12 @@ if (length(args) < 1) {
 source("../sim-scenario.R")
 library(wei.series.md.c1.c2.c3)
 N <- c(90L)
-P <- rep(c(.1, .25, .4, .55, .7), 100)
+P <- c(0.0)
 Q <- c(.825)
 
 sim_scenario(
     theta = alex_weibull_series$theta,
-    N = N, P = P, Q = Q, R = 200L, B = 1000L,
+    N = N, P = P, Q = Q, R = 400L, B = 1000L,
     max_iter = 125L, max_boot_iter = 125L,
     n_cores = 2L, csv_file = csv_file,
     ci_method = "bca", ci_level = .95)
